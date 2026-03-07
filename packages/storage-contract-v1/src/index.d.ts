6deb4223d43c9f15fa896e108d78b4316a9d68d2
Arthur Aleksandro Alves Silva <arthursilva.dev@gmail.com>
2026-03-07 17:04:50 +0000

    chore(contracts): centralize v1 capability interfaces and shared SQL schemas

--- packages/storage-contract-v1/src/index.d.ts
+++ packages/storage-contract-v1/src/index.d.ts
@@ -1,4 +1,4 @@
 export { runStorageV1Conformance } from "./conformance.js";
 export * from "./schema.js";
 export * from "./types.js";
-
+//# sourceMappingURL=index.d.ts.map
--- packages/sync-contract-v1/src/index.js
+++ packages/sync-contract-v1/src/index.js
@@ -1,3 +1,3 @@
 export { runSyncV1Conformance } from "./conformance.js";
 export * from "./types.js";
-
+//# sourceMappingURL=index.js.map
