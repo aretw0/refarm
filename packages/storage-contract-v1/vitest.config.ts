238322cba8a89e04e22cad83576d45ca10d8e382
Arthur Aleksandro Alves Silva <arthursilva.dev@gmail.com>
2026-03-22 11:19:36 +0000

    chore(storage-contract-v1): update vitest.config.ts

--- packages/storage-contract-v1/vitest.config.ts
+++ packages/storage-contract-v1/vitest.config.ts
@@ -1,5 +1,5 @@
 import { mergeConfig, defineConfig } from "vitest/config";
-import { baseConfig, getAliases } from "../../vitest.config";
+import { baseConfig, getAliases } from "@refarm.dev/vtconfig";
 import path from "node:path";
 
 export default mergeConfig(
