36379b7be248cdea6ef8e82d35ba5085ef50bc09
Arthur Aleksandro Alves Silva <arthursilva.dev@gmail.com>
2026-03-22 11:20:02 +0000

    chore(toolbox): update vitest.config.ts

--- packages/toolbox/vitest.config.ts
+++ packages/toolbox/vitest.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig, mergeConfig } from 'vitest/config';
-import baseConfig from '../../vitest.config';
+import baseConfig from '@refarm.dev/vtconfig';
 
 export default mergeConfig(
   baseConfig,
