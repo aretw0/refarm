16d498ce25207754297405d14733d49b823af7d0
Arthur Aleksandro Alves Silva <arthursilva.dev@gmail.com>
2026-04-09 02:59:20 +0000

    refactor: ajustar formatação de código e dependências em arquivos de configuração e scripts

--- validations/sqlite-benchmark/browser/vite.config.ts
+++ validations/sqlite-benchmark/browser/vite.config.ts
@@ -1,19 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  assetsInclude: ["**/*.wasm"],
   server: {
     headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
     },
   },
   preview: {
     headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
     },
   },
   optimizeDeps: {
-    exclude: ['@sqlite.org/sqlite-wasm'],
+    exclude: ["@sqlite.org/sqlite-wasm"],
   },
 });
