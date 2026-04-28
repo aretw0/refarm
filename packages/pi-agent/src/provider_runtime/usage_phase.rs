d0eb916a4cb3c8733abf3101d665e8cbfa253057
Arthur Aleksandro Alves Silva <arthursilva.dev@gmail.com>
2026-04-28 15:16:54 +0000

    refactor(pi-agent): split usage extraction primitives

--- packages/pi-agent/src/provider_runtime/usage_phase.rs
+++ packages/pi-agent/src/provider_runtime/usage_phase.rs
@@ -1,18 +1,7 @@
-use super::{AnthropicIterationPhase, OpenAiIterationPhase, UsageTotals};
-
-pub(crate) fn response_usage(response: &serde_json::Value) -> &serde_json::Value {
-    &response["usage"]
-}
-
-pub(crate) fn ingest_usage_from_response_with<F>(
-    totals: &mut UsageTotals,
-    response: &serde_json::Value,
-    mut ingest: F,
-) where
-    F: FnMut(&mut UsageTotals, &serde_json::Value),
-{
-    ingest(totals, response_usage(response));
-}
+use super::{
+    usage_extract::ingest_usage_from_response_with, AnthropicIterationPhase, OpenAiIterationPhase,
+    UsageTotals,
+};
 
 pub(crate) fn anthropic_phase_after_usage(
     totals: &mut UsageTotals,
