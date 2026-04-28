50be134e8196bf9c45461eb84bf840a8a3a2ebac
Arthur Aleksandro Alves Silva <arthursilva.dev@gmail.com>
2026-04-28 15:35:38 +0000

    refactor(pi-agent): split usage phase common primitive

--- packages/pi-agent/src/provider_runtime/usage_phase.rs
+++ packages/pi-agent/src/provider_runtime/usage_phase.rs
@@ -1,5 +1,5 @@
 use super::{
-    usage_extract::ingest_usage_from_response_with, AnthropicIterationPhase, OpenAiIterationPhase,
+    usage_phase_common::phase_after_usage_with, AnthropicIterationPhase, OpenAiIterationPhase,
     UsageTotals,
 };
 
@@ -25,18 +25,4 @@ pub(crate) fn openai_phase_after_usage(
         UsageTotals::ingest_openai_usage,
         super::openai_iteration_phase,
     )
-}
-
-pub(crate) fn phase_after_usage_with<P, FU, FP>(
-    totals: &mut UsageTotals,
-    response: &serde_json::Value,
-    ingest_usage: FU,
-    mut phase_from_response: FP,
-) -> P
-where
-    FU: FnMut(&mut UsageTotals, &serde_json::Value),
-    FP: FnMut(&serde_json::Value) -> P,
-{
-    ingest_usage_from_response_with(totals, response, ingest_usage);
-    phase_from_response(response)
 }
