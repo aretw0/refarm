95ea6c106b75cafa456ed3f83e67f02324bc4aae
Arthur Silva <arthursilva.dev@gmail.com>
2026-06-22 20:23:04 +0000

    refactor(refarm): remove command-result wrapper and test

--- apps/refarm/src/commands/command-result.ts
+++
@@ -1,7 +0,0 @@
-export {
-	commandPayloadNextActions,
-	commandPayloadNextCommands,
-	commandPayloadOk,
-	commandPayloadRecommendations,
-	parseCommandJsonPayload,
-} from "@refarm.dev/cli/command-result";
